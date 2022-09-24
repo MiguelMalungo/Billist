5dabd9a8c0ace828432766f86167c27fd6979e8e
JOSE MIGUEL FERRAZ GUEDES <josemiguelferrazguedes@JOSEs-MacBook-Pro.local>
2022-09-24 16:27:14 +0100

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+        String driver;
+        int age;
+
+        public Driver(String driver, int age) {
+            this.driver = driver;
+            this.age = age;
+        }
+
+    }
+
---
+++ src/Vehicle.java
@@ -0,0 +1,13 @@
+
+
+    public abstract class Vehicle {
+        Driver driver;
+
+        public void drive(){
+        }
+
+        public void setDriver(Driver driver) {
+        }
+
+    }
+
